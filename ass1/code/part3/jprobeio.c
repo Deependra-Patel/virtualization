a6fbfe5597c474715e6c9724383d6c34f5e23eb1
Deependra Pate <pateldeependra06@gmail.com>
2015-08-07 01:51:57 +0530

    Removed useless lines

--- ass1/code/part3/jprobeio.c
+++ ass1/code/part3/jprobeio.c
@@ -1,42 +1,24 @@
 /* jprobebio.c
-   This is a simple module to get information about block io operations.
-   Will Cohen
+  Deependra Patel 120050032
 */
 
 #include <linux/module.h>
 #include <linux/kprobes.h>
 #include <linux/bio.h>
 #include <linux/blkdev.h>
-#include <linux/hash.h>
 #include <linux/list.h>
-
-#define LOG_2_BINS 8
-#define BINS (1<<LOG_2_BINS)
 #define numPids (1<<15)
 
-static int count_generic_make_request = 0;
-static unsigned long long sectors_transferred = 0;
-
-static struct block_device *bin_bdev[BINS];
-static dev_t bin_bd_dev[BINS];
-static int bin_count[BINS];
-static unsigned long long bin_sectors[BINS];
-
+//Struct used for storing statistic data
 struct info{
-	bool called;
+	bool called;//If process makes a READ/WRITE request
 	int nRead;
 	int totalRead;
 	int nWrite;
 	int totalWrite;
-	//struct list_head list;
 };
 
 struct info infoArr[numPids];
-
-//struct info infoList;
-//LIST_HEAD_INIT(&infoList.list);
-//LIST_HEAD(infoList);
-//INIT_LIST_HEAD(&infoList.list);
 
 void populateData(int pid, int moreRead, int moreReadData, int moreWrite, int moreWriteData){
 	if(pid > numPids)
@@ -50,34 +32,10 @@ void populateData(int pid, int moreRead, int moreReadData, int moreWrite, int mo
 
 static void inst_generic_make_request(struct bio *bio)
 {
-  	unsigned long b=hash_ptr(bio->bi_bdev, LOG_2_BINS);
-
-  	++count_generic_make_request;
-  	sectors_transferred += bio_sectors(bio);
 	if((bio->bi_rw & 1) == WRITE)
 		populateData(current->pid, 0, 0, 1, bio->bi_iter.bi_size);
 	else if((bio->bi_rw & 1) == READ)
 		populateData(current->pid, 1, bio->bi_iter.bi_size, 0, 0);
-//	printk("Process: %d", current->pid);
-
-	/*
-	struct info newinfo = {
-		.pid = current->pid,
-		.nRead = 0,
-		.totalRead = 0,
-		.nWrite = 0,
-		.totalWrite = 0,
-		.list = LIST_HEAD_INIT(newinfo.list),
-	};*/
-//	struct info* temp = (struct info*)kmalloc(sizeof(struct info));
-//	list_add_tail(&newinfo.list, &infoList.list);
-	
-  	if( bin_bdev[b] == NULL || bin_bdev[b] == bio->bi_bdev) {
-	  bin_bdev[b] = bio->bi_bdev;
-	  bin_bd_dev[b] = bio->bi_bdev->bd_dev;
-	  ++bin_count[b];
-	  bin_sectors[b] += bio_sectors(bio);
-  	}
   	jprobe_return();
 }
 
@@ -105,19 +63,9 @@ void printData(void){
 
 void cleanup_module(void)
 {
-  int b;
-printData();
-  unregister_jprobe(&my_jprobe);
-  printk("jprobe unregistered\n");
-/*
-  printk("generic_make_request() called %d times for %lld sectors.\n",
-	 count_generic_make_request, sectors_transferred);
-  for (b=0; b<BINS; ++b)
-    if (bin_bdev[b] != NULL) 
-      printk("bdev 0x%p (%d,%d) %d %lld sectors.\n", bin_bdev[b],
-	     MAJOR(bin_bd_dev[b]), MINOR(bin_bd_dev[b]),
-	     bin_count[b], bin_sectors[b]);*/
+	printData();
+  	unregister_jprobe(&my_jprobe);
+	printk("jprobe unregistered\n");
 }
 
-MODULE_LICENSE("GPL");
 
